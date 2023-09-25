4028c23a374d3918c216db4924857024f3ae5582
Andrei Burvel <burvelandrei@gmail.com>
2023-09-25 16:48:45 +0300

    v1.03_bad_test

--- main.py
+++ main.py
@@ -31,14 +31,18 @@ user:dict = {
 #Реализуем хэндлер /start.
 @dp.message(CommandStart())
 async def process_start(message:Message):
-    await message.answer('''Привет!!! Я бот "Угадай число". Правила игры таковы я загадываю число,
-                         а ты его должен угадать за выбранное тобой количество попыток.
-                         Если вдруг ты забудешь правила введи /help.''')
+    await message.answer(f'Привет!!! Я бот "Угадай число". Правила игры таковы я загадываю число,'
+                         f'а ты его должен угадать за выбранное тобой количество попыток.\n'
+                         f'Если вдруг ты забудешь правила введи /help.')
 
 #Реализуем хэндлер /help.
 @dp.message(Command(commands='help'))
 async def process_help(message:Message):
-    await message.answer('h')
+    await message.answer(f'Этот бот служит для развлекательных целей.\n'
+                         f'Основная задача бота - это игра в "Угадай число".\n'
+                         f'Для старта игры введи "Да", "Игра", "Сыграем".\n'
+                         f'Для выхода из игры введи /cancel.\n'
+                         f'Для вывода статистики введите /stat.')
 
 #Реализуем хэндлер /stat - для вывода статистики пользователя.
 @dp.message(Command(commands='stat'))
@@ -56,12 +60,55 @@ async def process_cancel(message:Message):
         await message.answer('Мы ещё не играем.')
 
 #Реализуем функцию по согласию пользователя на игру.
-@dp.message(F.text.lower().in_['да', 'давай', 'сыграем', 'игра', 'давай сыграем', 'сыграть'])
+@dp.message(F.text.lower().in_(['да', 'давай', 'сыграем', 'игра', 'давай сыграем', 'сыграть']))
 async def process_yes(message:Message):
     if not user['in_game']:
         user['in_game']=True
-        user['attempts']==ATTEMPTS
-        
+        user['attempts']=ATTEMPTS
+        user['secret_number']=random_number()
+        await message.answer('Я так рад с тобой сыграть.')
+    else:
+        await message.answer('Мы с Вами уже так играем. Введите число от 1 до 100.')
+
+#Реализуем функцию по отказу пользователя от игры.
+@dp.message(F.text.lower().in_(['нет', 'не хочу', 'в другой раз', 'не буду']))
+async def process_no(message:Message):
+    if not user['in_game']:
+        await message.answer(f'Очень жаль. Я очень хочу с тобой сыграть.'
+                             f'Если захочешь сыграть просто введи \'Игра\'')
+    else:
+        await message.answer('Мы с Вами уже так играем. Введите число от 1 до 100.')
+
+#Реализуем игровой процесс.
+@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
+async def process_guess_the_number(message:Message):
+    if user['in_game']:
+        if user['secret_number']==int(message.text):
+            user['count_game']+=1
+            user['count_win']+=1
+            user['in_game']=False
+            message.answer('Ура!!! Ты угадал!')
+        elif user['secret_number']>int(message.text):
+            user['attempts']-=1
+            message.answer('Загаданное число больше')
+        elif user['secret_number']<int(message.text):
+            user['attempts']-=1
+            message.answer('Загаданное число меньше')
+        if user['attempts']==0:
+            user['in_game']=False
+            user['count_game']+=1
+            message.answer('Ты не угадал. Попробуй ещё раз.')
+    else:
+        message.answer('Мы еще не играем.')
+
+#Функция которая отлавливает все оставшиеся варианты.
+@dp.message()
+async def process_all(message:Message):
+    if user['in_game']:
+        message.answer(f'Мы с Вами играем. Введите число от 1 до 100.'
+                       f'Или команду /cancel.')
+    else:
+        message.answer('Я могу только играть в игру "Угадай число"')
 
 if __name__=='__main__':
     dp.run_polling(bot)
